bf4b6aee5083f3a8cb4d551de16768f881f830b5
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-01-20 12:14:05 +0100

    added dontMind and dontGiveA****

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# npm-ts-template
-A template for a npm project using typescript (and tsup)
+# dont-care
+
+Don't want to comment out code when debugging? Use dont-care!
+
+## I.dontCare
+
+```ts
+I.dontCare(() => {
+  console.log("This code won't be executed");
+});
+```
+
+## I.dontMind
+
+```ts
+I.dontMind(() => {
+  console.log(
+    "This code will be executed, but runtime errors will never appear"
+  );
+});
+
+I.dontMind(() => {
+  console.log("Add true to log the suppressed errors");
+}, true);
+```
+
+## I.dontGiveA\*\*\*\*
+
+```ts
+I["dontGiveA****"](() => {
+  console.log(
+    "Use this if a teammate wants changes to your code, use this a the change"
+  );
+});
+```
--- src/I.class.ts
+++ src/I.class.ts
@@ -1,9 +1,32 @@
 export class I {
+  /**
+   * I don't care
+   * @param callback Code that won't be executed
+   */
   static dontCare(callback: () => void) {}
+
+  /**
+   * I don't mind
+   * @param callback Code that will be executed, but will never error
+   * @param silent If true, runtime errors will not be logged, defaults to true
+   */
+  static dontMind(callback: () => void, silent = true) {
+    try {
+      callback();
+    } catch (error) {
+      if (!silent) {
+        console.error(error);
+      }
+    }
+  }
+
+  static "dontGiveA****"(callback: () => void) {
+    callback();
+  }
 }
 
 export const dontCare = I.dontCare;
 
-dontCare(() => {
-  console.log("I don't care");
+I["dontGiveA****"](() => {
+  console.log("I don't give a ****");
 });
